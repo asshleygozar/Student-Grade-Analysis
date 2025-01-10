06a5c1976806c04f27bb38e114c3b56b76fc727c
Asshley <asshleydeanwaynegozar@gmail.com>
2025-01-10 21:07:13 +0800

    Refactor source code structure

--- student_grade_analysis.py
+++ student_grade_analysis.py
@@ -5,20 +5,26 @@ import functools
 class StudentGradeAnalysis:
     
     def __init__(self, file_path):
+
         self.file_path = file_path
     
     def load_students_grades(self):
+
         try:
             with open(self.file_path, "r") as file:
                students =  json.load(file)
+
         except FileNotFoundError:
             students = []
+
         except json.JSONDecodeError:
             students = []
             print("Error loading json file. Starting with empty lists")
+
         return students
     
     def input_grades(self,load_file):
+
         try:
             while True:
                 print("Press 1 to exit")
@@ -40,21 +46,27 @@ class StudentGradeAnalysis:
             print("Student grade must be integer!")
 
     def student_grades(grades):
+
         data = instance.load_students_grades()
         total_grades = list(map(lambda x:"Name: " + str(x["name"]) + " Grade: "+str(x["grade"]) + "%", data))
         print("Students grades")
+
         for grade in total_grades:
             print(grade)
     
     def passed_students(grades):
+
         data = instance.load_students_grades()
         passed_grades = list(filter(lambda y: y["grade"] >= 75, data))
+
         for passed in passed_grades:
             print(f"Name: {passed["name"]} Grade: {passed["grade"]} Status: Passed!")
 
     def failed_students(grades):
+
         data = instance.load_students_grades()
         failed_grades = list(filter(lambda z: z["grade"] <75, data))
+
         for failed in failed_grades:
             print(f"Name: {failed["name"]} Grade: {failed["grade"]} Status: Failed!")
 
@@ -67,6 +79,7 @@ class StudentGradeAnalysis:
         print(f"Average grade of the class: {average}")
 
     def results():
+
         load = instance.load_students_grades()
         StudentGradeAnalysis.student_grades(load)
         StudentGradeAnalysis.passed_students(load)
@@ -75,6 +88,6 @@ class StudentGradeAnalysis:
 
 instance = StudentGradeAnalysis("students_grades.json")
 StudentGradeAnalysis.results()
-# instance.input_grades(instance.load_students_grades()) Function to input student name and grades
+
 
 
